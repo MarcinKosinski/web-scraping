cb8cdc4b32503b27c860130d64b6d5f343768e71
Marta <sommerm@student.mini.pw.edu.pl>
2015-03-14 20:36:53 +0100

    poprawka superfunkcji

--- Marta.R
+++ Marta.R
@@ -12,20 +12,21 @@ superfunkcja <- function(onet, dictionary){
     sum((x %in% dictionary) >= 1)
   })
   
-  t <- tytuly[which(unlist(lista)==1)]
+  t <- unique(tytuly[which(unlist(lista)==1)])
   l <- unlist(linki[which(unlist(lista)==1)])
   ll <- na.omit(unlist(stri_extract_all_regex(l, "[h][t][t][p].+")))
   
   data.frame(tytul=t, link=ll)
 }
 
-dictionary2 <- c("bronisław", "komorowski",
+dictionary <- c("bronisław", "komorowski",
                 "andrzej", "duda",
                 "magdalena", "ogórek")
 
-onet2 <- "http://www.onet.pl/"
+onet <- "http://www.onet.pl/"
+onet <- "http://www.tvn24.pl/"
 
-tab <- superfunkcja(onet2, dictionary2)
+tab <- superfunkcja(onet, dictionary)
 
 
 
