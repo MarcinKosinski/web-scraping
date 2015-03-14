dc1f8e1093b9f5c05ca7fa2c5da7e5796d115c9d
Marta <sommerm@student.mini.pw.edu.pl>
2015-03-14 20:30:23 +0100

    superfunkcja Marty

---
+++ Marta.R
@@ -0,0 +1,63 @@
+library("stringi")
+library("rvest")
+
+superfunkcja <- function(onet, dictionary){
+  
+  onet <- html(onet)
+  wszystko <- html_nodes(onet, "a")
+  linki <- html_attrs(wszystko)
+  tytuly <- html_text(wszystko)
+  
+  lista <- lapply(lapply(stri_extract_all_words(tytuly), stri_trans_tolower), function(x){
+    sum((x %in% dictionary) >= 1)
+  })
+  
+  t <- tytuly[which(unlist(lista)==1)]
+  l <- unlist(linki[which(unlist(lista)==1)])
+  ll <- na.omit(unlist(stri_extract_all_regex(l, "[h][t][t][p].+")))
+  
+  data.frame(tytul=t, link=ll)
+}
+
+dictionary2 <- c("bronisław", "komorowski",
+                "andrzej", "duda",
+                "magdalena", "ogórek")
+
+onet2 <- "http://www.onet.pl/"
+
+tab <- superfunkcja(onet2, dictionary2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
