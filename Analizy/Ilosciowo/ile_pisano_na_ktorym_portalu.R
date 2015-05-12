4a00ce296856e431250250471e81fb1e570961b0
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-05-12 19:43:02 +0200

    Nasza apka wyglada coraz lepiej :)!!

---
+++ Analizy/Ilosciowo/ile_pisano_na_ktorym_portalu.R
@@ -0,0 +1,136 @@
+library(dplyr)
+
+
+paths2articles <- paste0( "dane/artykuly/", 
+                          grep( ".txt", list.files("dane/artykuly/"), value = TRUE))
+
+paths2articles %>% 
+   sapply( function( element ){
+      readLines( element, n = 4 ) -> x
+      x[grep("http", x)[1]]
+   }) %>% 
+   unique() -> result
+      
+      
+#      length(result)
+
+
+
+data.frame(
+Komorowski = length(grep("komorows", result)),
+Ogorek = length(grep("ogorek", result)),
+Duda = length(grep("dud", result)),
+Mikke = length(grep("mikke|korwin", result))
+) -> howManyArticles
+
+library(reshape2)
+howManyArticles <- melt(howManyArticles)
+names(howManyArticles) <- c("kandydat", "ileArtykulow")
+
+library(ggplot2)
+library(ggthemes)
+
+motyw <- theme_bw(base_family = "serif", base_size = 28) +
+   theme(legend.background = element_blank(),
+         legend.key = element_blank(),
+         panel.background = element_blank(),
+         panel.border = element_blank(),
+         strip.background = element_blank(),
+         plot.background = element_blank(),
+         axis.line = element_blank(),
+         panel.grid = element_blank(),
+         legend.position = "top",
+         axis.title.x = element_text(vjust=-0.4),
+         axis.title.y = element_text(vjust=+1)
+   )
+
+# ggplot(howManyArticles, aes( y = ileArtykulow, x = kandydat))+
+#    geom_bar(stat="identity") + motyw +
+#    ylab("ile artykułów") + ggtitle("Ile się pisze ostatnio \n o kandydatch w wyborach?")
+
+
+
+##### domens
+PROTOCOL_REGEX <- "^(?:(?:[[:alpha:]+.-]+)://)?"
+PREFIX_REGEX <- "(?:www\\.)?"
+HOSTNAME_REGEX <- "([^/]+)"
+REST_REGEX <- ".*$"
+URL_REGEX <- paste0(PROTOCOL_REGEX, PREFIX_REGEX, HOSTNAME_REGEX, REST_REGEX)
+domain.name <- function(urls) gsub(URL_REGEX, "\\1", urls)
+domain.name(result)
+
+
+library(stringi)
+
+result2 <- grepl("http", result)
+
+lapply( strsplit( result, split = "http"), function(element){
+   grep( x = element, pattern = "//", value = TRUE )
+}
+) %>% unlist -> linki
+
+sapply( linki, function(element){
+   paste0( "http", element ) %>% domain.name
+}
+) -> domens
+   
+   
+
+
+helpMe <- character( length(result) )
+
+helpMe[grepl("komorows", result)] <- "Komorowski"
+helpMe[grepl("ogorek", result)] <- "Ogorek"
+helpMe[grepl("dud", result)] <- "Duda"
+helpMe[grepl("mikke|korwin", result)] <- "Mikke"
+        
+
+data.frame( kto = helpMe, domena =unlist(domens)) -> WhoAndWhere
+
+WhoAndWhere %>% filter( kto != "") -> WhoAndWhere
+
+
+WhoAndWhere
+WhoAndWhere[, 2] <- as.character(WhoAndWhere[, 2])
+
+najczesciej_piszacy <- names(tail(sort(table(WhoAndWhere[, 2])), 5))
+
+WhoAndWhere %>%
+   filter( domena %in% najczesciej_piszacy ) %>%   
+   group_by( kto, domena) %>% 
+   summarise( count = n()) -> WhoAndWhere2Viz
+
+# WhoAndWhere2Viz
+# 
+# ggplot(WhoAndWhere2Viz, aes( y = count, x = domena))+
+#    geom_bar(stat="identity") + motyw +
+#    ylab("ile artykułów") + ggtitle("Które najczęściej piszące \n o wyborach portale piszą \n o jakich kandydatch w wyborach?")+
+#    facet_grid(kto~.) + theme_bw(base_family = "serif", base_size = 18) +
+#    theme(legend.background = element_blank(),
+#          legend.key = element_blank(),
+#          panel.background = element_blank(),
+#          panel.border = element_blank(),
+#          strip.background = element_blank(),
+#          plot.background = element_blank(),
+#          axis.line = element_blank(),
+#          panel.grid = element_blank(),
+#          legend.position = "top",
+#          axis.title.x = element_text(vjust=-0.4),
+#          axis.title.y = element_text(vjust=+1),
+#          axis.text.x = element_text(vjust=0.2,angle = 45)
+#    )
+
+
+# require(devtools)
+# install_github('rCharts', 'ramnathv')
+
+# library(rCharts)
+# n1 <- nPlot( count ~ domena, group = "kto", data = WhoAndWhere2Viz,
+#              type = "multiBarChart")
+
+
+
+
+WhoAndWhere2Viz %>% as.data.frame() -> WhoAndWhere2Viz
+
+save(WhoAndWhere2Viz, file = "Analizy/Ilosciowo/barchart.rda")
--- Aplikacja/app.R
+++ Aplikacja/app.R
@@ -2,57 +2,95 @@
 library(shinydashboard)
 library(shiny)
 library(dygraphs)
+library(rCharts)
+library(dplyr)
 ui <- dashboardPage(
    dashboardHeader(title = "Wybory w internecie"),
    dashboardSidebar(
       sidebarMenu(
          menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-         menuItem("Opis", tabName = "opis", icon = icon("th"))
+         menuItem("Opis", tabName = "opis", icon = icon("th")),
+         menuItem("Kody źródłowe", icon = icon("file-code-o"),
+                  href = "https://github.com/MarcinKosinski/web-scraping"
+         )
       )
    ),
    dashboardBody(
       tabItems(
-         # First tab content
-         tabItem(tabName = "dashboard",
-      # Boxes need to be put in a row (or column)
-      fluidRow(
-         box(plotOutput("plot1", height = 250)),
+               # First tab content
+          tabItem(tabName = "dashboard",
+                  # Boxes need to be put in a row (or column)
+                  fluidRow(
+#                      box(plotOutput("plot1", height = 250)),
+#                      
+#                      box(
+#                         title = "Controls",
+#                         sliderInput("slider", "Number of observations:", 1, 100, 50)
+#                      ),
+#                      hr(),
+                     box(title = "Analiza sentymentu", collapsible = TRUE, 
+                          width = 4, solidHeader = TRUE, status = "warning",
+                        dygraphOutput("dajgraf", height = 400)
+                     ),
+
+                      box(title = "Kto o kim pisał?", collapsible = TRUE, 
+                          width = 8, solidHeader = TRUE, status = "warning",
+                         showOutput("myChart", "nvd3")
+                      ),
+                     box(title = "Legenda", textOutput("legendDivID"), collapsible = TRUE,
+                         collapsed = TRUE,
+                         width = 4, solidHeader = TRUE, status = "warning", height = 20),
+                     box(title = "Kierunek osi", collapsible = TRUE, 
+                         collapsed = TRUE,
+                         width = 8, solidHeader = TRUE, status = "warning",  height = 20,
+                         selectInput(inputId = "type",
+                                     label = "Kierunek osi wykresu paskowego",
+                                     choices = c("multiBarChart", "multiBarHorizontalChart"),
+                                     selected = "multiBarChart")
+                     )
+                     
+                  )
+         ),
          
-         box(
-            title = "Controls",
-            sliderInput("slider", "Number of observations:", 1, 100, 50)
-         ),   
-         box(
-            dygraphOutput("dajgraf", height = 400)
-         )   
-         
-      )
-   ),
-   
-   tabItem(tabName = "opis",
-           h2("To aplikacja Marty Sommer i Marcina Kosinskiego"),
-           h5("Proba pisania heheszki")
+         tabItem(tabName = "opis",
+                 h2("To aplikacja Marty Sommer i Marcina Kosinskiego"),
+                 h5("Proba pisania heheszki")
+               )
+         )
    )
-      )
-)
 )
 
 load("Analizy/Sentyment/doNarysowaniaDygraph.rda")
+load("Analizy/Ilosciowo/barchart.rda")
 
 server <- function(input, output) {
-   set.seed(122)
-   histdata <- rnorm(500)
-   
-   output$plot1 <- renderPlot({
-      data <- histdata[seq_len(input$slider)]
-      hist(data)
-   })
-   
+#    set.seed(122)
+#    histdata <- rnorm(500)
+#    
+#    output$plot1 <- renderPlot({
+#       data <- histdata[seq_len(input$slider)]
+#       hist(data)
+#    })
+#    
    
    output$dajgraf <- renderDygraph({
-      dygraph( doNarysowaniaDygraph, main = "Analiza sentymentu" ) %>%
-         dyAxis("y", label = "Emocje" )
+      dygraph( doNarysowaniaDygraph ) %>%
+         dyAxis("y", label = "Emocje" ) %>% 
+         dyRangeSelector() %>%
+         dyLegend(labelsDiv = "legendDivID") %>%
+         dyOptions(colors = c('brown', 'blue', '#594c26', 'green') )
    })
+   
+   output$myChart <- renderChart({
+      
+      n1 <- nPlot( count ~ domena, group = "kto", data = WhoAndWhere2Viz,
+                   type = input$type)
+      n1$addParams(dom = "myChart")
+      n1$chart(color = c('brown', '#594c26', 'blue',  'green'))
+      return(n1)
+      
+   })
+   
    
 }
 
