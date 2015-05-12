414dacc9b7e9022fbd4c2b2479070dd9251695de
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-05-12 17:42:43 +0200

    poprawka w indicators.R

--- indicators/indicatorsArticles.R
+++ indicators/indicatorsArticles.R
@@ -7,7 +7,7 @@ paths2articles <- paste0( "dane/artykuly/",
 paths2articles %>% 
    sapply( function( element ){
       readLines( element, n = 4 ) -> x
-      x[2]
+      x[grep("http", x)[1]]
    }) %>% 
    unique() -> result
       
@@ -19,7 +19,7 @@ paths2articles %>%
 data.frame(
 Komorowski = length(grep("komorows", result)),
 Ogorek = length(grep("ogorek", result)),
-Duda = length(grep("duda|dudzie", result)),
+Duda = length(grep("dud", result)),
 Mikke = length(grep("mikke|korwin", result))
 ) -> howManyArticles
 
@@ -51,19 +51,37 @@ ggplot(howManyArticles, aes( y = ileArtykulow, x = kandydat))+
 
 
 ##### domens
+PROTOCOL_REGEX <- "^(?:(?:[[:alpha:]+.-]+)://)?"
+PREFIX_REGEX <- "(?:www\\.)?"
+HOSTNAME_REGEX <- "([^/]+)"
+REST_REGEX <- ".*$"
+URL_REGEX <- paste0(PROTOCOL_REGEX, PREFIX_REGEX, HOSTNAME_REGEX, REST_REGEX)
+domain.name <- function(urls) gsub(URL_REGEX, "\\1", urls)
+domain.name(result)
+
 
 library(stringi)
-stri_extract_all_words(result) %>% 
-   lapply( function(element){
-      element[3]
-   }) -> domens
+
+result2 <- grepl("http", result)
+
+lapply( strsplit( result, split = "http"), function(element){
+   grep( x = element, pattern = "//", value = TRUE )
+}
+) %>% unlist -> linki
+
+sapply( linki, function(element){
+   paste0( "http", element ) %>% domain.name
+}
+) -> domens
+   
+   
 
 
 helpMe <- character( length(result) )
 
 helpMe[grepl("komorows", result)] <- "Komorowski"
 helpMe[grepl("ogorek", result)] <- "Ogorek"
-helpMe[grepl("duda|dudzie", result)] <- "Duda"
+helpMe[grepl("dud", result)] <- "Duda"
 helpMe[grepl("mikke|korwin", result)] <- "Mikke"
         
 
@@ -72,13 +90,21 @@ data.frame( kto = helpMe, domena =unlist(domens)) -> WhoAndWhere
 WhoAndWhere %>% filter( kto != "") -> WhoAndWhere
 
 
-WhoAndWhere %>% 
+WhoAndWhere
+WhoAndWhere[, 2] <- as.character(WhoAndWhere[, 2])
+
+powyzej3 <- table(WhoAndWhere[, 2])[ table(WhoAndWhere[, 2]) > 3] %>% names()
+
+WhoAndWhere %>%
+   filter( domena %in% powyzej3 ) %>%   
    group_by( kto, domena) %>% 
    summarise( count = n()) -> WhoAndWhere2Viz
 
+WhoAndWhere2Viz
+
 ggplot(WhoAndWhere2Viz, aes( y = count, x = domena))+
    geom_bar(stat="identity") + motyw +
-   ylab("ile artykułów") + ggtitle("Które portale piszę \n o jakich kandydatch w wyborach?")+
+   ylab("ile artykułów") + ggtitle("Które 10 najczęściej piszących \n o wyborach portali piszę \n o jakich kandydatch w wyborach?")+
    facet_grid(kto~.) + theme_bw(base_family = "serif", base_size = 18) +
    theme(legend.background = element_blank(),
          legend.key = element_blank(),
