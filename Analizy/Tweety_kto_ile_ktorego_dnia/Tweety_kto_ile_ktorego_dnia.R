67ef709938763837c47f7fb82eae5a99c3d2e431
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-05-13 22:53:53 +0200

    drugi wykres do analizy tweetow

--- Analizy/Tweety_kto_ile_ktorego_dnia/Tweety_kto_ile_ktorego_dnia.R
+++ Analizy/Tweety_kto_ile_ktorego_dnia/Tweety_kto_ile_ktorego_dnia.R
@@ -33,3 +33,52 @@ melt( tabela ) -> tabela2
 
 names(tabela2) <- c("dzien", "kto", "ile_tweetow")
 save(tabela2, file = "D:/web-scraping/Aplikacja/tabela2.rda")
+
+
+
+
+
+bb %>%
+   filter(duda_indykatror == TRUE) -> to  
+as.data.frame(100*sort(table(to$urzadzenie), decreasing = TRUE)[1:5]/nrow(to)) -> to           
+colnames(to) <- "ile"        
+to$gdzie <- rownames(to)
+rownames(to) <- NULL
+to$indykator <- "duda"
+duda <- to
+
+bb %>%
+   filter(komorowski_indykator == TRUE) -> to  
+as.data.frame(100*sort(table(to$urzadzenie), decreasing = TRUE)[1:5]/nrow(to)) -> to           
+colnames(to) <- "ile"        
+to$gdzie <- rownames(to)
+rownames(to) <- NULL
+to$indykator <- "komorowski"
+komorowski <- to
+
+bb %>%
+   filter(ogorek_indykator == TRUE) -> to  
+as.data.frame(100*sort(table(to$urzadzenie), decreasing = TRUE)[1:5]/nrow(to)) -> to           
+colnames(to) <- "ile"        
+to$gdzie <- rownames(to)
+rownames(to) <- NULL
+to$indykator <- "ogorek"
+ogorek <- to
+
+bb %>%
+   filter(kukiz_indykator == TRUE) -> to  
+as.data.frame(100*sort(table(to$urzadzenie), decreasing = TRUE)[1:5]/nrow(to)) -> to           
+colnames(to) <- c("ile")        
+to$gdzie <- rownames(to)
+rownames(to) <- NULL
+to$indykator <- "kukiz"
+kukiz <- to
+
+tab <- rbind(duda, komorowski, ogorek, kukiz)
+
+save(tab, file = "D:/web-scraping/Aplikacja/tab.rda")
+
+
+# ggplot(tab, aes(x=gdzie, y=ile, fill=indykator))+
+#    geom_bar(stat="identity", position="dodge")+
+#    theme_bw()
